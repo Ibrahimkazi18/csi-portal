01b492bf73c5defd00ee8c420627a4e490231c85
Ibrahim <ibirfkazi@gmail.com>
2026-02-11 19:38:43 +0530

    fix ts utils/supabase error

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { type CookieOptions, createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
@@ -26,7 +26,11 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: {
+          name: string
+          value: string
+          options?: CookieOptions
+        }[]) {
           cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
             request,
